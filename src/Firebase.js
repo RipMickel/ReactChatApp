a3b81293cbaffafd5423f7a3ec2c0716f0959aa9
RipMickel <gaudicosmickelangelo@gmail.com>
2025-04-26 15:41:52 +0800

    added item on gitignore

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,12 +5,12 @@ import { getDatabase } from 'firebase/database';
 
 // Firebase configuration object
 const firebaseConfig = {
-    apiKey: "AIzaSyA0RO5INjD4xD3xjiHSdAzg8MbV3e9k44A",
-    authDomain: "messengerchatapp-789f1.firebaseapp.com",
-    projectId: "messengerchatapp-789f1",
-    storageBucket: "messengerchatapp-789f1.firebasestorage.app",
-    messagingSenderId: "672159984202",
-    appId: "1:672159984202:web:674d3ef364c6409835d4b3"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
 
 // Initialize Firebase
